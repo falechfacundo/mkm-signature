defccd51ada8671c3deb519fee30d5b30c0a0dac
jorgem <jorgem@jorgem.pc>
2026-05-21 07:10:07 -0300

    build(vercel): add official Astro Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   site: 'https://onboarding.ad-astra.me',
+
   integrations: [
     react(),
   ],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: 'static',
+  adapter: vercel(),
 });
